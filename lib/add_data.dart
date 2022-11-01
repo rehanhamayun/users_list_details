ab9f6615874f3827a254cc29ead5065b7fb75f1c
Rehan Hamayun <rr8329203@gmail.com>
2022-11-01 16:51:45 +0500

    initial commit

---
+++ lib/add_data.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:sqf_lite/sqflite_database/database.dart';
+import 'package:sqf_lite/sqflite_database/model.dart';
+
+import 'main.dart';
+
+class AddData extends StatefulWidget {
+  const AddData({Key? key}) : super(key: key);
+
+  @override
+  State<AddData> createState() => _AddDataState();
+}
+
+class _AddDataState extends State<AddData> {
+  DBHelper? dbHelper;
+  var title = TextEditingController();
+  var age = TextEditingController();
+  var description = TextEditingController();
+  var email = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        toolbarHeight: 89,
+        title: const Text('Add Data'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: title,
+              decoration: InputDecoration(
+                hintText: 'Title',
+                fillColor: const Color(0xffe9e9e9),
+                filled: true,
+                border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: email,
+              decoration: InputDecoration(
+                hintText: 'email',
+                fillColor: const Color(0xffe9e9e9),
+                filled: true,
+                border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: age,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Age',
+                fillColor: const Color(0xffe9e9e9),
+                filled: true,
+                border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: description,
+              maxLines: 4,
+              minLines: 2,
+              decoration: InputDecoration(
+                hintText: 'Description',
+                fillColor: const Color(0xffe9e9e9),
+                filled: true,
+                border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Colors.green),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurple,
+                minimumSize: const Size(250, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: const Text(
+                'Add Data',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                dbHelper
+                    ?.insert(NotesModel(
+                  title: title.text,
+                  age: int.parse(age.text.toString()),
+                  email: email.text,
+                  description: description.text,
+                ))
+                    .then((value) {
+                  if (kDebugMode) {
+                    print('Data Added');
+                  }
+                }).onError((error, stackTrace) {
+                  if (kDebugMode) {
+                    print(error);
+                  }
+                });
+                title.clear();
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MyHomePage()));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:sqf_lite/sqflite_database/database.dart';
+import 'package:sqf_lite/sqflite_database/model.dart';
+
+import 'add_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My SqfLite',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<NotesModel>> noteList;
+  DBHelper? dbHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper();
+    loadData();
+  }
+
+  navigator() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const AddData()));
+  }
+
+  loadData() {
+    noteList = dbHelper!.getCartListWithUserId();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My SQF Lite DataBase'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: FutureBuilder(
+        future: noteList,
+        builder: (context, AsyncSnapshot<List<NotesModel>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  direction: DismissDirection.startToEnd,
+                  background: Container(
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.red,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                      ],
+                    ),
+                  ),
+                  key: ValueKey<int>(snapshot.data![index].id!),
+                  child: Dismissible(
+                    direction: DismissDirection.endToStart,
+                    background: Container(
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.red,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                            size: 50,
+                          ),
+                        ],
+                      ),
+                    ),
+                    key: ValueKey<int>(snapshot.data![index].id!),
+                    onDismissed: (DismissDirection direction) {
+                      setState(() {
+                        dbHelper?.deleteProduct(snapshot.data![index].id!);
+                        noteList = dbHelper!.getCartListWithUserId();
+                        snapshot.data?.remove(snapshot.data![index]);
+                      });
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.all(20),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)),
+                      //shadowColor: Colors.purpleAccent,
+                      elevation: 1,
+                      semanticContainer: true,
+                      child: SizedBox(
+                          height: 90,
+                          child: Center(
+                              child: ListTile(
+                            title: Text(
+                              snapshot.data![index].title +
+                                  '        (' +
+                                  snapshot.data![index].email.toString() +
+                                  ')',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            subtitle: Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text(
+                                snapshot.data![index].description,
+                              ),
+                            ),
+                            trailing: Text(
+                              snapshot.data![index].age.toString(),
+                            ),
+                          ))),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: navigator,
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
